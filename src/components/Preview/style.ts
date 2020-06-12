fd1a0d6bac6b6618560c6cc22bd74839f7b14d03
이성진 <chlskatnr1@naver.com>
2020-06-12 18:12:10 +0900

    [CREATE] <src/components/Preview/*, src/components/PreviewMain/*> 프리뷰 추가
    2020.06.12

---
+++ src/components/Preview/style.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Preview = styled.div`
+  
+`;
---
+++ src/components/PreviewMain/PreviewMain.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+
+}
+
+const PreviewMain: React.FC<Props> = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default PreviewMain
