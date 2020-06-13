3639031b6a2187e10c5d60e34aa6368d994d7fd8
이성진 <chlskatnr1@naver.com>
2020-06-13 19:27:58 +0900

    [MODIFY] <Main.tsx> 메인 오류 고침
    2020.06.13

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import * as S from './style';
-import { Preview } from '../';
-import pencilSvg from '../../assets/icon/pencil.svg';
-import plusSvg from '../../assets/icon/plus.svg';
+import { Preview, Inbox } from '../';
 
 interface Props { }
 
@@ -12,31 +10,9 @@ const Main: React.FC<Props> = () => {
       <S.MainHeader>
         <Preview />
       </S.MainHeader>
-      <section id="inbox">
-        <h3>목록</h3>
-        <ul>
-
-          <li>
-            <div className="img-wrap">
-              <img src={pencilSvg} alt="pencil" title="pencil" />
-            </div>
-            <div className="info">
-              <h3>Final design</h3>
-              <p>Inuse Exprience</p>
-            </div>
-            <div className="until"><span>9am</span></div>
-          </li>
-
-        </ul>
-        <div id="bottom-wrap">
-          <div id="completed-wrap">
-            <span>COMPLETED</span><span id="completed-num">5</span>
-          </div>
-          <img id="plus" src={plusSvg} alt="plus" title="plus" />
-        </div>
-      </section>
+      <Inbox />
     </S.Main>
-  );
-};
+  )
+}
 
 export default Main;
