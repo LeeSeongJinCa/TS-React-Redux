3d26e570200aedd02d898a5a74bc25161b3c896b
이성진 <chlskatnr1@naver.com>
2020-06-13 19:22:48 +0900

    [MODIFY] <components/Inbox/Inbox.tsx> 인 박스 컴포넌트
    2020.06.13

---
+++ src/components/Inbox/Inbox.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import * as S from './style';
+import InboxInputList from '../InboxInputList/InboxInputList';
+import plusSvg from '../../assets/icon/plus.svg';
+
+interface Props { }
+
+const Inbox: React.FC<Props> = () => {
+  return (
+    <S.Inbox>
+      <S.InboxTitle>목록</S.InboxTitle>
+      <InboxInputList />
+      <div id="bottom-wrap">
+        <div id="completed-wrap">
+          <span>COMPLETED</span><span id="completed-num">5</span>
+        </div>
+        <img id="plus" src={plusSvg} alt="plus" title="plus" />
+      </div>
+    </S.Inbox>
+  );
+};
+
+export default Inbox;
