0d5a7a43d5b752a1aa0417d44802d09798f00bc0
이성진 <chlskatnr1@naver.com>
2020-06-15 11:21:29 +0900

    [MODIFY] <components/TodoHeader/*> Todo 헤더 부분 -> TodoHeader 컴포넌트 화
    2020.06.15

---
+++ src/components/TodoHeader/TodoHeader.tsx
@@ -0,0 +1,32 @@
+import React, { useRef } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  notSvg,
+  undoSvg,
+} from '../../assets';
+import * as S from './style';
+interface Props {
+  imgRef: React.MutableRefObject<any>;
+}
+
+const Todo: React.FC<Props> = ({ imgRef }) => {
+  return (
+    <S.TodoHeader>
+      <S.TodoHeaderNav>
+        <Link to="/">
+          <S.TodoHeaderNavImg
+            src={undoSvg}
+            alt="undo"
+            title="undo"
+          />
+        </Link>
+        <S.TodoHeaderNavTiitle>Add new thing</S.TodoHeaderNavTiitle>
+      </S.TodoHeaderNav>
+      <S.TodoHeaderShownImgWrap>
+        <S.TodoHeaderShownImg ref={imgRef} src={notSvg} alt="study" title="study" />
+      </S.TodoHeaderShownImgWrap>
+    </S.TodoHeader>
+  );
+};
+
+export default Todo;
