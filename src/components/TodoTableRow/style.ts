7eeb75bfdd04baf9c4f0306f2939f0ff957511ca
이성진 <chlskatnr1@naver.com>
2020-06-15 11:23:16 +0900

    [ADD] <components/TodoTableRow/*> TodoTableRow 컴포넌트 추가
    2020.06.15

---
+++ src/components/TodoTableRow/style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import {
+  globalComplementaryColor,
+} from '../../GlobalStyle';
+
+export const TodoInputTableRow = styled.div`
+  position: relative;
+  margin-bottom: 40px;
+  border-bottom: 1px solid ${globalComplementaryColor};
+`;
