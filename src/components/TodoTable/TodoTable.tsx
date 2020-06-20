3d29d068f29c0a48bcbc0fcf441299a33b7eb989
이성진 <chlskatnr1@naver.com>
2020-06-20 18:18:33 +0900

    [ADD] <components/TodoTable/*> TodoCalendar 추가
    2020.06.20

--- src/components/TodoTable/TodoTable.tsx
+++ src/components/TodoTable/TodoTable.tsx
@@ -1,4 +1,8 @@
-import React, { useMemo, Dispatch } from 'react';
+import React, {
+  useMemo,
+  Dispatch,
+  ReactElement,
+} from 'react';
 import * as S from './style';
 import {
   TodoTableRow,
@@ -20,6 +24,8 @@ const TodoTable: React.FC<Props> = ({
   todoState,
   todoDispatch,
 }) => {
+  const today: Date = new Date();
+
   const tableRows: React.ReactElement[] = useMemo(() => {
     return todoInputList.map(({ id }: IInputs, i) =>
       <TodoTableRow
@@ -32,6 +38,63 @@ const TodoTable: React.FC<Props> = ({
     );
   }, []);
 
+  const getDateHTML = (
+    styling: string = '',
+    children: number = 0,
+    id: string = '',
+  ): any => (
+    <S.CalendarDay key={id} className={`calendar__day ${styling}`}>
+      <S.CalendarDaySpan id={id}>{children ? children : ''}</S.CalendarDaySpan>
+    </S.CalendarDay>
+  );
+
+  const setFixDayCount = (num: number): string => num < 10 ? `0${num}` : `${num}`;
+
+  const setCalendarData = (todayCopy: Date, year: number, copyMonth: number): ReactElement[] => {
+    const month: string = setFixDayCount(copyMonth),
+      lastDay: number = new Date(year, +month, 0).getDate(),
+      firstDayName: number = new Date(year, +month - 1, 1).getDay(),
+      firstDayThisWeek: number = todayCopy.getDate() - todayCopy.getDay(),
+      calJSX: ReactElement[] = [];
+    let startDayCount: number = 1;
+
+    for (let i = 0; i < 6; i += 1) {
+      for (let j = 0; j < 7; j += 1) {
+        if (i === 0 && j < firstDayName) {
+          calJSX.push(getDateHTML('', 0, `${j}`));
+        } else if (startDayCount >= firstDayThisWeek
+          && startDayCount < firstDayThisWeek + 7
+          && startDayCount <= lastDay) {
+          if (startDayCount === todayCopy.getDate()) {
+            calJSX.push(getDateHTML('week today', startDayCount, `${year}${month}${setFixDayCount(startDayCount)}`));
+          } else {
+            calJSX.push(getDateHTML('week', startDayCount, `${year}${month}${setFixDayCount(startDayCount)}`));
+          }
+          startDayCount += 1;
+        } else if (i > 0 && startDayCount <= lastDay) {
+          calJSX.push(getDateHTML('month', startDayCount, `${year}${month}${setFixDayCount(startDayCount)}`));
+          startDayCount += 1;
+        }
+      }
+    }
+
+    return calJSX;
+  };
+
+  const memoizedCalendar = useMemo<ReactElement[]>(
+    () => setCalendarData(today, today.getFullYear(), (today.getMonth() + 1))
+  , []);
+
+  const setCalendarDayData = useMemo(() => {
+    const dayArr: string[] = ['일', '월', '화', '수', '목', '금', '토'];
+    const dayJsx = dayArr.map(id => (
+      <S.CalendarDay className="by" key={id}>
+        <S.CalendarDaySpan>{id}</S.CalendarDaySpan>
+      </S.CalendarDay>),
+    );
+    return dayJsx;
+  }, []);
+
   return (
     <S.TodoInputTable>
       <TodoTableType
@@ -39,9 +102,10 @@ const TodoTable: React.FC<Props> = ({
         todoDispatch={todoDispatch}
       />
       {tableRows}
-      <input type="date" name="date" id="date" /> {/* "2020-06-20" */}
-      <input type="time" name="time" id="time" /> {/* "22:41" */}
-      {/* new Date(`${d} ${t}`); */}
+      <S.Calendar>
+        {setCalendarDayData}
+        {memoizedCalendar}
+      </S.Calendar>
     </S.TodoInputTable>
   );
 };
