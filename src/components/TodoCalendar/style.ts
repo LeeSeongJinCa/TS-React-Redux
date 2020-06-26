f0f65912963b24a0e6a41cc6dfc23ebcbaa9115e
이성진 <chlskatnr1@naver.com>
2020-06-26 22:40:44 +0900

    [REMOVE] <components/Todo**> Remove All About TodoCalendar and other
    2020.06.26

--- src/components/TodoCalendar/style.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Calendar = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  margin: auto;
-  height: 480px;
-`;
--- src/components/TodoCalendarSetting/style.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const CalnedarDaySetting = styled.div`
-  margin-top: 50px;
-  text-align: center;
-`;
