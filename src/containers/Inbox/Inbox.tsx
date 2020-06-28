dd914adb99a724da0911298421d51708136ad0d2
이성진 <chlskatnr1@naver.com>
2020-06-28 23:09:39 +0900

    [MODIFY] <🎫/Inbox, InboxInputList> Todo 가져오기, 삭제 기능 추가
    2020.06.28

--- src/containers/Inbox/Inbox.tsx
+++ src/containers/Inbox/Inbox.tsx
@@ -4,51 +4,55 @@ import React, {
   useEffect,
   useRef,
   MutableRefObject,
-  ReactElement,
   Dispatch,
   SetStateAction,
+  ReactElement,
+  MouseEvent,
 } from 'react';
+import axios from 'axios';
+
+import { GetIInput } from '../../static/todoForm';
+import { URL } from '../../static/server';
 import { Inbox, InboxInput, InboxNoList } from '../../components';
 import {
   InboxInputListContainer,
   InboxBottomContainer,
 } from '../../containers';
-import { URL } from '../../static/server';
-import axios from 'axios';
 
-interface Props {
-
-}
-
-interface IInputs {
-  type: string;
-  thing: string;
-  time: number;
-  notification: string;
-}
+interface Props { }
 
 const InboxContainer: React.FC<Props> = () => {
   const [inputs, setInputs]:
-    [IInputs[], Dispatch<SetStateAction<IInputs[]>>] = useState([]);
+    [GetIInput[], Dispatch<SetStateAction<GetIInput[]>>] = useState([]);
   const isExistInput: MutableRefObject<any> = useRef(undefined);
 
-  const inputList: ReactElement[] = useMemo(
-    () => inputs.map((input: IInputs, i) => {
-      return (<InboxInput
-        key={i}
-        type={input.type}
-        thing={input.thing}
-        time={input.time}
-        notification={input.notification}
-      />);
-    },
-  ), [inputs]);
+  const getTodo = async () => {
+    const get = await axios.get(`${URL}/todos`);
+    setInputs(get.data);
+  };
+
+  const inputList: ReactElement[] = useMemo(() => inputs.map((input: GetIInput) => {
+    const { _id, type, thing, notification, endDate } = input;
+    const handleDeleteInbox = async () => {
+      if (!confirm('삭제하시겠습니까?')) { return; }
+      try {
+        await axios.delete(`${URL}/todos/${_id}`);
+        const filterInputs = inputs.filter((o: GetIInput) => o._id !== _id);
+        setInputs(filterInputs);
+      } catch { }
+    };
+    return (<InboxInput
+      key={_id}
+      type={type}
+      thing={thing}
+      notification={notification}
+      endDate={endDate}
+      handleDeleteInbox={handleDeleteInbox}
+    />);
+  }), [inputs]);
 
   useEffect(() => {
-    axios.get(`${URL}/todos`)
-      .then((data) => {
-        setInputs(data.data);
-      });
+    getTodo();
   }, []);
   useEffect(() => {
     isExistInput.current = inputList;
