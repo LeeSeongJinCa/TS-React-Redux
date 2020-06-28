5c02a0e080b8c1ce085bffa6ae23f32c7d2695b8
이성진 <chlskatnr1@naver.com>
2020-06-28 22:50:58 +0900

    [MODIFY] <🎨/CalendaerSetting/*> remove TitleInput
    2020.06.28

--- src/components/CalendarSetting/style.ts
+++ src/components/CalendarSetting/style.ts
@@ -25,28 +25,6 @@ export const CalendarSettingButton = styled.button`
     inset 0 0 5px #ffffff;
 `;
 
-export const CalendarSettingTitleLabel = styled.label`
-  display: inline-block;
-`;
-
-export const CalendarSettingTitleInput = styled.input`
-  padding: 8px 12px;
-  border: 2px solid #f2f2f3;
-  border-radius: 8px;
-  font-size: 20px;
-  box-shadow: 0 0 3px #f2f2f2,
-    inset 2px 3px 4px #cdcdcd;
-  &:focus {
-    border-color: #67a7ec
-  }
-  &.error {
-    border-color: #e27e7f;
-  }
-  &.confirmed {
-    border-color: #7cd184;
-  }
-`;
-
 export const CalendarSettingSelectedWrap = styled.div`
   margin: 4px 0;
 `;
