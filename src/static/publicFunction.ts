bf0b77539e9b5c23fa03fb07190ea62cb0101708
이성진 <chlskatnr1@naver.com>
2020-06-28 23:04:17 +0900

    [MODIFY] <✨/publicFunction.ts, todoForm.ts> add GetIInput, add pad fun
    2020.06.28

--- src/static/publicFunction.ts
+++ src/static/publicFunction.ts
@@ -1,8 +1,10 @@
+export const pad = (num: number): string => num < 10 ? `0${num}` : `${num}`;
+
 export const getDetailTime = (utcTime) => {
   const date = new Date(utcTime);
   const yyyy = date.getFullYear();
   const mm = date.getMonth();
   const dd = date.getDate();
-
-  return `${yyyy}년 ${mm}월 ${dd}일 까지`;
+  return `${yyyy}-${pad(mm)}-${pad(dd)}`;
 };
+
