bf89823a3a2a43b8ffe29cdefc9df66851eb6399
이성진 <chlskatnr1@naver.com>
2020-06-28 23:11:46 +0900

    [MODIFY] <modules/schedule.ts> Reset 기능 추가
    2020.06.28

--- src/modules/schedule.ts
+++ src/modules/schedule.ts
@@ -3,20 +3,24 @@ import { ThunkAction } from 'redux-thunk';
 
 export const STARTDATE = 'STARTDATE' as const;
 export const ENDDATE = 'ENDDATE' as const;
+export const RESET = 'RESET' as const;
 
 export const setStartDate = (date: string) => ({
   type: STARTDATE,
   payload: { date },
 });
-
 export const setEndDate = (date: string) => ({
   type: ENDDATE,
   payload: { date },
 });
+export const setReset = () => ({
+  type: RESET,
+})
 
 type ScheduleAction =
   | ReturnType<typeof setStartDate>
-  | ReturnType<typeof setEndDate>;
+  | ReturnType<typeof setEndDate>
+  | ReturnType<typeof setReset>;
 
 export type ScheduleState = {
   startDate: string;
@@ -36,6 +40,14 @@ export const setScheduleThunk: ActionCreator<ThunkAction<
 >> = (date: string, setFn: (date: string) => ScheduleAction) => (dispatch) => {
   return dispatch(setFn(date));
 };
+export const setResetThunk: ActionCreator<ThunkAction<
+  ScheduleAction,
+  ScheduleState,
+  void,
+  any
+>> = (setFn: () => ScheduleAction) => (dispatch) => {
+  return dispatch(setFn());
+};
 
 const schedule = (state = initialState, action: ScheduleAction): ScheduleState => {
   switch (action.type) {
@@ -48,6 +60,12 @@ const schedule = (state = initialState, action: ScheduleAction): ScheduleState =
       return {
         ...state,
         endDate: action.payload.date,
+      };
+    case RESET:
+      return {
+        ...state,
+        startDate: '',
+        endDate: '',
       };
     default:
       return state;
