3d191991078c7cdcb90b4a9f7365fd9bae25ee4f
이성진 <chlskatnr1@naver.com>
2020-07-05 16:05:50 +0900

    [MODIFY] <🎨/PreviewMain/*> add menu and link movie.tsx
    2020.07.05

--- src/components/PreviewMain/PreviewMain.tsx
+++ src/components/PreviewMain/PreviewMain.tsx
@@ -1,23 +1,65 @@
-import * as React from 'react';
-import * as S from './style';
-import hamburgerIconSvg from '../../assets/icon/hamburgerIcon.svg';
+import React, { useMemo, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-interface Props {}
+import * as S from './style';
+import {
+  hamburgerMenuSvg,
+  movieSvg,
+} from '../../assets';
+
+interface Props { }
+
+type MenuType = {
+  img: string,
+  item: string,
+};
 
 const PreviewMain: React.FC<Props> = () => {
+  const menus = useMemo<MenuType[]>(() => [
+    {
+      img: movieSvg,
+      item: 'Movie',
+    }, {
+      img: movieSvg,
+      item: 'Goal',
+    }, {
+      img: movieSvg,
+      item: 'Aim',
+    },
+  ], []);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggleOpend = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const getMenus = useMemo(
+    () => menus.map((m: MenuType) => (<Link to="/movie" key={m.item}>
+      <S.PreviewNavMenuItem>
+        <S.PreviewNavMenuItemImg src={m.img} alt={m.item} title={m.item} />
+        <S.PreviewNavMenuItemText>{m.item}</S.PreviewNavMenuItemText>
+      </S.PreviewNavMenuItem>
+    </Link>)),
+    [],
+  );
+
   return (
     <S.PreviewMain>
-      <div>
-        <S.PreviewHamburgerMenu
-          src={hamburgerIconSvg}
+      <S.PreviewNav>
+        <S.PreviewNavMenu
+          src={hamburgerMenuSvg}
           alt="hamburgerIcon"
           title="hamburgerIcon"
+          onClick={toggleOpend}
         />
-      </div>
+        {isOpen
+          && <S.PreviewNavMenuList>{getMenus}</S.PreviewNavMenuList>
+        }
+      </S.PreviewNav>
       <h1>Your <br /> Things</h1>
       <span>2020년 6월 12일</span>
     </S.PreviewMain>
-  )
-}
+  );
+};
 
 export default PreviewMain;
