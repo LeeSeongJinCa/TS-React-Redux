38a3233a6ba20ab0451ea5183d4a97bca5e66a5e
이성진 <chlskatnr1@naver.com>
2020-07-05 16:09:50 +0900

    [CREATE] <🎨/Movie/*> create movie component
    2020.07.05

---
+++ src/components/Movie/Movie.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './style';
+
+interface Props { }
+
+const Movie: React.FC<Props> = () => {
+  return (
+    <S.Movie>Test</S.Movie>
+  );
+};
+
+export default Movie;
