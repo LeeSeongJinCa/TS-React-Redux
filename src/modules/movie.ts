dc9bf6397f99cfe37bda05aae35abf47179e9927
이성진 <chlskatnr1@naver.com>
2020-07-06 22:47:55 +0900

    [MODIFY] <🎁/movie.ts> add Movie api interface
    2020.07.06

--- src/modules/movie.ts
+++ src/modules/movie.ts
@@ -1,38 +1,86 @@
 import { ActionCreator } from 'redux';
 import { ThunkAction } from 'redux-thunk';
 
-import { apiGetGenre } from '../utils';
+import { apiGetGenre, apiGetList } from '../utils';
 
-type GenreType = {
+interface GenreType {
   id: number;
   name: string;
-};
+}
 
-export const FETCHGENRE = 'GENRE' as const;
-export const LOADINGGENRE = 'LOADINGGENRE' as const;
+interface IMovieItem {
+  poster_path: string;
+  popularity: number;
+  vote_count: number;
+  video: boolean;
+  media_type: string;
+  id: number;
+  adult: boolean;
+  backdrop_path: string;
+  original_language: string;
+  original_title: string;
+  genre_ids: number[];
+  title: string;
+  vote_average: number;
+  overview: string;
+  release_date: string;
+}
 
+interface IMovieList {
+  created_by: string;
+  description: string;
+  favorite_count: number;
+  id: string;
+  items: IMovieItem[];
+}
+
+export const FETCH_LIST = 'FETCHLIST' as const;
+export const FETCH_GENRE = 'GENRE' as const;
+export const LOADING = 'LOADING' as const;
+
+export const fetchList = (list: IMovieList) => ({
+  type: FETCH_LIST,
+  payload: { list },
+});
 export const fetchGenre = (genres: GenreType[]) => ({
-  type: FETCHGENRE,
+  type: FETCH_GENRE,
   payload: { genres },
 });
 export const loadingGenre = () => ({
-  type: LOADINGGENRE,
+  type: LOADING,
 });
 
 type MovieAction =
+  | ReturnType<typeof fetchList>
   | ReturnType<typeof fetchGenre>
   | ReturnType<typeof loadingGenre>;
 
 export type MovieState = {
+  list: IMovieList | {};
   genres: GenreType[];
   loading: boolean;
 };
 
 const initailState: MovieState = {
+  list: {},
   genres: [],
   loading: false,
 };
 
+export const fetchListThunk: ActionCreator<ThunkAction<
+  Promise<void>,
+  MovieAction,
+  null,
+  MovieAction
+>> = (listId: number) => async (dispatch) => {
+  dispatch(loadingGenre());
+  try {
+    const res = await apiGetList(listId);
+    dispatch(fetchList(res.data));
+  } catch (err) {
+    throw err;
+  }
+};
 export const fetchGenreThunk: ActionCreator<ThunkAction<
   Promise<void>,
   MovieAction,
@@ -50,13 +98,19 @@ export const fetchGenreThunk: ActionCreator<ThunkAction<
 
 const movie = (state = initailState, action: MovieAction): MovieState => {
   switch (action.type) {
-    case FETCHGENRE:
+    case FETCH_LIST:
+      return {
+        ...state,
+        list: action.payload.list,
+        loading: false,
+      };
+    case FETCH_GENRE:
       return {
         ...state,
         genres: action.payload.genres,
         loading: false,
       };
-    case LOADINGGENRE:
+    case LOADING:
       return {
         ...state,
         loading: true,
