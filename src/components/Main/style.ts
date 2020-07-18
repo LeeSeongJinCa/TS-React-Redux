0c6ae78018b2f4b51e51514626a400e23b362b66
이성진 <chlskatnr1@naver.com>
2020-07-18 11:00:33 +0900

    [MODIFY] connect MovieItem, GenereList, GenreItem

--- src/components/Main/style.ts
+++ src/components/Main/style.ts
@@ -6,30 +6,4 @@ import {
 export const Main = styled.div`
   box-shadow: 3px 3px 10px #777777;
   span { color: #b6b6b6; }
-  #bottom-wrap {
-    position: relative;
-    > #completed-wrap {
-      display: inline-block;
-      margin-top: 12px;
-      font-size: 14px;
-      > #completed-num {
-        display: inline-block;
-        width: 25px;
-        height: 25px;
-        margin-left: 12px;
-        line-height: 25px;
-        text-align: center;
-        border-radius: 50%;
-        color: white;
-        background-color: ${globalBackColor};
-      }
-    }
-    > img {
-      position: absolute;
-      top: 0;
-      right: 0;
-      width: 45px;
-      height: 45px;
-    }
-  }
 `;
